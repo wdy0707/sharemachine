28b6346bce1c93dc67478872933cd9593ff03fb9
王丹阳 <1150828564@qq.com>
2019-07-09 15:06:56 +0800

    修改了init的一些bug:包括mybatis整合包和逆向工程别名alias不兼容的问题

--- sharemachine/src/main/java/top/wdy07/sharemachine/mapper/UserMapper.java
+++ sharemachine/src/main/java/top/wdy07/sharemachine/mapper/UserMapper.java
@@ -1,15 +1,32 @@
 package top.wdy07.sharemachine.mapper;
 
-import top.wdy07.sharemachine.entity.User;
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Component;
+import top.wdy07.sharemachine.entity.User;
+import top.wdy07.sharemachine.entity.UserExample;
+@Mapper
+@Component
 public interface UserMapper {
+    int countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
     int deleteByPrimaryKey(String uid);
 
     int insert(User record);
 
     int insertSelective(User record);
 
+    List<User> selectByExample(UserExample example);
+
     User selectByPrimaryKey(String uid);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
 
     int updateByPrimaryKeySelective(User record);
 
---
+++ sharemachine/src/main/java/top/wdy07/sharemachine/service/UserTestService.java
@@ -0,0 +1,23 @@
+package top.wdy07.sharemachine.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import top.wdy07.sharemachine.entity.User;
+import top.wdy07.sharemachine.entity.UserExample;
+import top.wdy07.sharemachine.mapper.UserMapper;
+
+/**
+ * @author wdy
+ * @create 2019-07-09 13:51
+ */
+@Service
+public class UserTestService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public User getUserById(String id){
+        return userMapper.selectByPrimaryKey(id);
+    }
+
+}
