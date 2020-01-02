026e629f874afb917abbc1e2a27d076918e35018
王丹阳 <1150828564@qq.com>
2020-01-02 19:13:57 +0800

    mkm

--- sharemachine/src/main/java/top/wdy07/sharemachine/SharemachineApplication.java
+++ sharemachine/src/main/java/top/wdy07/sharemachine/SharemachineApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SharemachineApplication {
 
     public static void main(String[] args) {
+        //
         SpringApplication.run(SharemachineApplication.class, args);
     }
 
